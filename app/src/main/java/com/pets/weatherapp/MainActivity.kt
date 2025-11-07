069968876888023681f49e4aa7b5b48a02d7d398
Andrey Sidorov <cugopob.a@yandex.ru>
2025-11-07 18:41:46 +0700

    Added SplashScreen

--- app/src/main/java/com/pets/weatherapp/MainActivity.kt
+++ app/src/main/java/com/pets/weatherapp/MainActivity.kt
@@ -4,16 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.pets.weatherapp.presentation.navigation.AppNavGraph
 import com.pets.weatherapp.presentation.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
+    private var keepSplashOnScreen by mutableStateOf(true)
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().setKeepOnScreenCondition {
+            keepSplashOnScreen
+        }
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             WeatherAppTheme {
                 AppNavGraph()
+                keepSplashOnScreen = false
             }
         }
     }
