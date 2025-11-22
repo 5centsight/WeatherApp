905db9355364385b291252316aeeaf6fbda88d53
Andrey Sidorov <cugopob.a@yandex.ru>
2025-11-22 17:00:23 +0700

    Added ConnectTimeOutException

--- app/src/main/java/com/pets/weatherapp/data/api/ApiClient.kt
+++ app/src/main/java/com/pets/weatherapp/data/api/ApiClient.kt
@@ -6,27 +6,31 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
 
 object ApiClient {
     private const val BASE_URL = "https://pogoda.ngs.ru/api/v1/"
-    val contentType = "application/json".toMediaType()
-    val json = Json {
+    private val contentType = "application/json".toMediaType()
+    private val json = Json {
         isLenient = true
         explicitNulls = false
         ignoreUnknownKeys = true
         coerceInputValues = true
     }
 
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .connectTimeout(10, TimeUnit.SECONDS)
+        .readTimeout(10, TimeUnit.SECONDS)
+        .writeTimeout(10, TimeUnit.SECONDS)
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(json.asConverterFactory(contentType))
-        .client(
-            OkHttpClient.Builder()
-                .addInterceptor(HttpLoggingInterceptor().apply {
-                    level = HttpLoggingInterceptor.Level.BODY
-                })
-                .build()
-        )
+        .client(okHttpClient)
         .build()
 
     val weatherService: ForecastApi by lazy {
